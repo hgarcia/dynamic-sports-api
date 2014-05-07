108eaf08b3ae73209e2cb0ec8deb137c21e669a1
hgarcia <hernan@dynamicprogrammer.com>
2014-05-07 11:06:36 -0400

    [HG] Adding a route for the root path

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var multiparty = require('connect-multiparty');
 var multipartyMiddleware = multiparty();
 var app = express();
 
+app.get("/", function (req, res) {
+	res.send("Hello uploader");
+});
+
 app.post("/uploads", multipartyMiddleware, function (req, res) {
 	console.log(req.body, req.files);
+	res.send(req.files);
 });
 
 var port = Number(process.env.PORT || 5000);

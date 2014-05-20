016305cc25f3087f4161cdc0fe1194e042069a4e
hgarcia <hernan@dynamicprogrammer.com>
2014-05-20 16:46:48 -0400

    [HG] Output file content to console.log

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var multiparty = require('connect-multiparty');
 var multipartyMiddleware = multiparty();
+var fs = require('fs');
 var app = express();
 
 app.get("/", function (req, res) {
-	res.send("Hello uploader");
+	var tpl = "<h3>Hello uploader</h3>";
+	tpl += "<form enctype='multipart/form-data' action='/uploads' method='POST'>";
+	tpl += "File: <input type='file' name='file'>";
+	tpl += "<input type='submit' value='Upload'>";
+	tpl += "</form>";
+	res.send(tpl);
 });
 
 app.post("/uploads", multipartyMiddleware, function (req, res) {
-	console.log(req.body, req.files);
+	var file = req.files.file;
+	var content = fs.readFileSync(file.path);
+	console.log(content.toString());
 	res.send(200);
 });
 
